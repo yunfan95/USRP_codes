6c06995a3f3f32c5388324d073c711111cfb2b0b
Yunfan Wang <49604253+yunfan95@users.noreply.github.com>
2022-08-25 11:20:31 -0400

    Update read.m

--- Step4_data_read/read.m
+++ Step4_data_read/read.m
@@ -1,1 +1,12 @@
+file_name = 'rx_leo.dat';
 
+fid = fopen(file_name);
+data = fread(fid, [2,inf],'float');
+yn = data(1,:) + 1i*data(2,:);
+fclose(fid);
+fs = 5e5;
+
+time = 1/fs : 1/fs : length(yn)/fs;
+hold on
+plot(time, real(yn))
+plot(time, imag(yn))
